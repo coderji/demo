bf1ea2b5505a07fe520bc90b49ac2877bf8a1ecc
ji.yang <ji.yang@tinno.com>
2019-04-02 13:10:44 +0800

    service

--- app/src/main/java/com/ji/algorithm/MainActivity.java
+++ app/src/main/java/com/ji/algorithm/MainActivity.java
@@ -2,12 +2,20 @@ package com.ji.algorithm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
+import com.ji.utils.LogUtils;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private static final int NOTIFICATION_ID = 10;
     private boolean mStart = false;
 
     @Override
@@ -26,5 +34,37 @@ public class MainActivity extends AppCompatActivity {
                 mStart = !mStart;
             }
         });
+    }
+
+    private void sendNotify(String name) {
+        LogUtils.v(TAG, "sendNotify");
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                Notification.Builder builder = new Notification.Builder(getApplicationContext());
+                builder.setContentText("ContentText1");
+                builder.setSmallIcon(R.mipmap.ic_launcher);
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    NotificationChannel notificationChannel =
+                            new NotificationChannel(getPackageName(), "Channel1", NotificationManager.IMPORTANCE_DEFAULT);
+                    notificationManager.createNotificationChannel(notificationChannel);
+                    builder.setChannelId(notificationChannel.getId());
+                }
+                notificationManager.notify(NOTIFICATION_ID, builder.build());
+            } else {
+                Notification notification = new Notification();
+                notification.tickerText = "TickerText1";
+                notificationManager.notify(NOTIFICATION_ID, notification);
+            }
+        }
+    }
+
+    private void cancelNotify() {
+        LogUtils.v(TAG, "cancelNotify");
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.cancel(NOTIFICATION_ID);
+        }
     }
 }
--- app/src/main/java/com/ji/algorithm/MainService.java
+++ app/src/main/java/com/ji/algorithm/MainService.java
@@ -6,14 +6,13 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.IBinder;
 
 import com.ji.utils.LogUtils;
 
 public class MainService extends Service {
     private static final String TAG = "MainService";
-    private static final int NOTIFICATION_ID = 0;
+    private static final int NOTIFICATION_ID = 20;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -25,21 +24,17 @@ public class MainService extends Service {
         super.onCreate();
         LogUtils.v(TAG, "onCreate");
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            Notification.Builder builder = new Notification.Builder(getApplicationContext(), getPackageName());
+            builder.setContentText("ContentText2");
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+
+            NotificationChannel notificationChannel = new NotificationChannel(getPackageName(), "Channel2", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setSound(null, null);
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             if (notificationManager != null) {
-                Notification.Builder builder = new Notification.Builder(getApplicationContext(), "ChannelId");
-                builder.setContentText("ContentText");
-                builder.setSmallIcon(R.mipmap.ic_launcher);
-
-                NotificationChannel notificationChannel =
-                        new NotificationChannel(getPackageName(), "ChannelName", NotificationManager.IMPORTANCE_DEFAULT);
                 notificationManager.createNotificationChannel(notificationChannel);
-                builder.setChannelId(notificationChannel.getId());
-
-                notificationManager.notify(NOTIFICATION_ID, builder.build());
-
-                startForeground(NOTIFICATION_ID, new Notification());
             }
+            startForeground(NOTIFICATION_ID, builder.build());
         }
     }
 
@@ -47,37 +42,8 @@ public class MainService extends Service {
     public void onDestroy() {
         super.onDestroy();
         LogUtils.v(TAG, "onDestroy");
-    }
-
-    private void sendNotify(String name) {
-        LogUtils.v(TAG, "sendNotify");
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        if (notificationManager != null) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                Notification.Builder builder = new Notification.Builder(getApplicationContext());
-                builder.setContentText("ContentText");
-                builder.setSmallIcon(R.mipmap.ic_launcher);
-
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                    NotificationChannel notificationChannel =
-                            new NotificationChannel(getPackageName(), "ChannelName", NotificationManager.IMPORTANCE_DEFAULT);
-                    notificationManager.createNotificationChannel(notificationChannel);
-                    builder.setChannelId(notificationChannel.getId());
-                }
-                notificationManager.notify(NOTIFICATION_ID, builder.build());
-            } else {
-                Notification notification = new Notification();
-                notification.tickerText = TAG;
-                notificationManager.notify(NOTIFICATION_ID, notification);
-            }
-        }
-    }
-
-    private void cancelNotify() {
-        LogUtils.v(TAG, "cancelNotify");
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        if (notificationManager != null) {
-            notificationManager.cancel(NOTIFICATION_ID);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            stopForeground(true);
         }
     }
 }
