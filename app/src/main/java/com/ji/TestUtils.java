0932ca174dbddf04d5a12a09a9f453b9cf99bbcf
ji.yang <ji.yang@tinno.com>
2020-09-15 13:59:38 +0800

    test

---
+++ app/src/main/java/com/ji/TestUtils.java
@@ -0,0 +1,63 @@
+package com.ji;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class TestUtils {
+    private static final String TAG = "TestUtils";
+    private int mDisable = 1;
+    private final Object mLock = new Object();
+
+    private void setDisabled(int disable) {
+        log("setDisabled disable:" + disable);
+        mDisable = disable;
+    }
+
+    private int getDisabled() {
+        return mDisable;
+    }
+
+    private void disable(int disable) {
+        synchronized (mLock) {
+            log("disable disable:" + disable);
+            sleep(100);
+            setDisabled(disable);
+        }
+    }
+
+    private void recomputeDisableFlags() {
+//        synchronized (mLock) {
+            int disable = getDisabled();
+            log("recomputeDisableFlags disable:" + disable);
+            disable(disable);
+//        }
+    }
+
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void log(String s) {
+        System.out.println(String.format(Locale.getDefault(), "%-10s", Thread.currentThread().getName())
+                + new SimpleDateFormat("MM-dd HH:mm:ss.SSS", Locale.getDefault()).format(new Date())
+                + " " + TAG + ": " + s);
+    }
+
+    public static void main(String[] args) {
+        TestUtils testUtils = new TestUtils();
+        new Thread(() -> {
+            testUtils.disable(0);
+        }).start();
+        sleep(50);
+        new Thread(() -> {
+            testUtils.recomputeDisableFlags();
+        }).start();
+        sleep(1000);
+        log("getDisabled:" + testUtils.getDisabled());
+    }
+}
