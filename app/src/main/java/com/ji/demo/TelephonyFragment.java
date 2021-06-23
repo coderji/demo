4e6207bf3c2df70b05506c69aeaccc7d0065fb66
ji.yang <ji.yang@tinno.com>
2021-06-23 17:24:57 +0800

    euicc

--- app/src/main/java/com/ji/demo/TelephonyFragment.java
+++ app/src/main/java/com/ji/demo/TelephonyFragment.java
@@ -6,11 +6,15 @@ import android.telecom.Call;
 import android.telecom.CallScreeningService;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.telephony.UiccCardInfo;
+import android.telephony.euicc.EuiccManager;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.ji.util.Log;
+
+import java.util.List;
 
 public class TelephonyFragment  extends Fragment {
     private static final String TAG = "TelephonyFragment";
@@ -18,6 +22,7 @@ public class TelephonyFragment  extends Fragment {
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
+        getEuiccManager(context);
     }
 
     private void listenCallState(Context context) {
@@ -28,6 +33,26 @@ public class TelephonyFragment  extends Fragment {
                 Log.d(TAG, "onCallStateChanged state:" + state + " phoneNumber:" + phoneNumber);
             }
         }, PhoneStateListener.LISTEN_CALL_STATE);
+    }
+
+    private void getEuiccManager(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        EuiccManager euiccManager = (EuiccManager) context.getSystemService(Context.EUICC_SERVICE);
+        EuiccManager euiccManager1 = null, euiccManager2 = null;
+        try {
+            List<UiccCardInfo> list = telephonyManager.getUiccCardsInfo();
+            for (UiccCardInfo info : list) {
+                Log.d(TAG, "info:" + info);
+                if (info.getSlotIndex() == 0) {
+                    euiccManager1 = euiccManager.createForCardId(info.getCardId());
+                } else if (info.getSlotIndex() == 1) {
+                    euiccManager2 = euiccManager.createForCardId(info.getCardId());
+                }
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "getUiccCardsInfo" + e);
+        }
+        Log.d(TAG, "euiccManager1:" + euiccManager1 + " euiccManager2:" + euiccManager2);
     }
 
     public static class CallScreeningServiceImpl extends CallScreeningService {
