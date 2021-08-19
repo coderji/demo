6b18b61186eeb2623239f5ff057f83a1a4d017cb
ji.yang <ji.yang@tinno.com>
2021-08-19 17:21:40 +0800

    resource

--- app/src/main/java/com/ji/demo/MainActivity.java
+++ app/src/main/java/com/ji/demo/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 
 import androidx.fragment.app.FragmentActivity;
 
+import com.ji.util.Log;
+
+import java.util.Arrays;
+
 public class MainActivity extends FragmentActivity {
     private static final String TAG = "MainActivity";
 
@@ -16,5 +20,14 @@ public class MainActivity extends FragmentActivity {
                 .beginTransaction()
                 .add(R.id.main_content, new SensorFragment())
                 .commit();
+    }
+
+    private void testResources() {
+        int id = getResources().getIdentifier("config_biometric_sensors", "array", "android");
+        Log.d(TAG, "testResources id:" + id);
+        if (id > 0) {
+            String[] config_biometric_sensors = getResources().getStringArray(id);
+            Log.d(TAG, "config_biometric_sensors:" + Arrays.toString(config_biometric_sensors));
+        }
     }
 }
