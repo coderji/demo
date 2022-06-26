9697b165435b4d123fc600d24bfd906ac957ad7c
ji.yang <ji.yang@wikophone.com>
2022-06-26 21:51:22 +0800

    merge

--- app/src/main/java/com/ji/jar/MergeLog.java
+++ app/src/main/java/com/ji/jar/MergeLog.java
@@ -14,82 +14,69 @@ public class MergeLog {
     private static final String TAG = "MergeLog";
     private static boolean DEBUG = false;
 
-    private static void mergeFile(String[] paths) {
-        log("mergeFile paths:" + Arrays.toString(paths));
-        int length = paths.length;
+    private static void mergeFile(String[] files) {
+        log("mergeFile files:" + Arrays.toString(files));
+        int length = files.length;
         FileReader[] frs = new FileReader[length];
         BufferedReader[] brs = new BufferedReader[length];
         String[] strings = new String[length];
-        int[] lines = new int[length];
-        int mergeLine = 0;
-        try {
-            int minIndex = 0;
-            for (int i = 0; i < length; i++) {
-                frs[i] = new FileReader(paths[i]);
+
+        for (int i = 0; i < length; i++) {
+            try {
+                frs[i] = new FileReader(files[i]);
                 brs[i] = new BufferedReader(frs[i]);
-                if (brs[i].ready()) {
-                    strings[i] = brs[i].readLine();
-                    lines[i] = 1;
-                    if (i != minIndex && compare(strings[i], strings[minIndex]) < 0) {
-                        minIndex = i;
-                    }
-                } else {
-                    log("mergeFile " + paths[i] + " not ready");
-                    brs[i].close();
-                    frs[i].close();
-                    lines[i] = 0;
-                }
+                strings[i] = brs[i].readLine();
+            } catch (IOException e) {
+                log("mergeFile read fail, " + e);
             }
+        }
+
+        try {
             FileWriter fw = new FileWriter("mergeLog.txt");
             BufferedWriter bw = new BufferedWriter(fw);
-
             while (true) {
-                if (DEBUG) log("mergeFile write minIndex:" + minIndex + " line:" + lines[minIndex] + " string:" + strings[minIndex]);
-                bw.write(strings[minIndex]);
-                bw.newLine();
-                mergeLine++;
-                if (brs[minIndex].ready()) {
-                    strings[minIndex] = brs[minIndex].readLine();
-                    attrLines[minIndex]++;
-                    for (int i = 0; i < attrs.length; i++) {
-                        if (i != minIndex && strings[i] != null && compare(strings[i], strings[minIndex]) < 0) {
-                            minIndex = i;
-                        }
-                    }
-                } else {
-                    log("mergeFile " + attrs[minIndex] + " done");
-                    strings[minIndex] = null;
-                    brs[minIndex].close();
-                    frs[minIndex].close();
-                    for (int i = 0; i < attrs.length; i++) {
-                        if (strings[i] != null) {
-                            minIndex = i;
-                        }
-                    }
-                    if (strings[minIndex] == null) {
-                        break;
-                    }
-                    for (int i = 0; i < attrs.length; i++) {
-                        if (i != minIndex && strings[i] != null && compare(strings[i], strings[minIndex]) < 0) {
-                            minIndex = i;
-                        }
-                    }
+                int min = findMin(strings);
+                if (min == -1) {
+                    break;
                 }
+                bw.write(strings[min]);
+                bw.newLine();
+                bw.flush();
+                strings[min] = brs[min].readLine();
             }
-            bw.flush();
             bw.close();
             fw.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            log("mergeFile write fail" + e);
         }
-        int allLine = 0;
-        StringBuilder stringBuilder = new StringBuilder("mergeFile ");
-        for (int i = 0; i < attrs.length; i++) {
-            allLine = allLine + attrLines[i];
-            stringBuilder.append(attrs[i]).append(":").append(attrLines[i]).append(" ");
+
+        for (int i = 0; i < length; i++) {
+            try {
+                brs[i].close();
+                frs[i].close();
+            } catch (IOException e) {
+                log("mergeFile close fail, " + e);
+            }
         }
-        stringBuilder.append("allLine:").append(allLine).append(" mergeLine:").append(mergeLine);
-        log(stringBuilder.toString());
+    }
+
+    private static int findMin(String[] strings) {
+        int length = strings.length;
+        int min = -1;
+        for (int i = 0; i < length; i++) {
+            if (strings[i] != null) {
+                min = i;
+                break;
+            }
+        }
+        if (min >= 0) {
+            for (int i = min + 1; i < length; i++) {
+                if (strings[i] != null && compare(strings[min], strings[i]) > 0) {
+                    min = i;
+                }
+            }
+        }
+        return min;
     }
 
     private static int compare(String a, String b) {
